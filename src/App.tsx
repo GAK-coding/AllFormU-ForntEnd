c97028b4f416be3c2ad3311b4d7a0f3a06d3c377
5hyun <qhslsl@gmail.com>
2023-04-04 22:51:37 +0900

    Feat: init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      프론트엔드
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div>프론트 세팅</div>
+    );
+};
