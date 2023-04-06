02230b0d403e9b7cdef65a41e6d72f3bc258071e
5hyun <qhslsl@gmail.com>
2023-04-06 14:30:04 +0900

    Feat: 홈페이지 완성

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import { Explanation, ExplanationAbove, ExplanationBelow, ExplanationMiddle, HomeWrapper, Img } from './styles';
+import Button from '../../components/ui/Button';
+import { useRecoilValue } from 'recoil';
+import { color } from '../../recoil/Color/atom';
 
 export default function Home() {
-  return <div>홈</div>;
+  const { main } = useRecoilValue(color);
+
+  return (
+    <HomeWrapper>
+      <Explanation>
+        <div>
+          <ExplanationAbove>
+            <span>Gak과 함께,</span>
+            <br /> 손쉽게 만드는 설문 플랫폼
+          </ExplanationAbove>
+          <ExplanationMiddle>All Form Ü</ExplanationMiddle>
+          <ExplanationBelow>
+            <Button
+              text={`나만의 설문 만들기 ->`}
+              fontSize={1.4}
+              bgColor={main}
+              width={20}
+              height={5}
+              color={'white'}
+            />
+          </ExplanationBelow>
+        </div>
+      </Explanation>
+      <Img>
+        <img src="logo.png" alt="logo" />
+      </Img>
+    </HomeWrapper>
+  );
 }
