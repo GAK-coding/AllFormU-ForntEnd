46d5569fcd5426541dd4cfac666575c4cacdbece
5hyun <qhslsl@gmail.com>
2023-04-06 15:17:41 +0900

    Feat: 회원가입 초기 설정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BtnBox, HeaderWrapper, Title } from './styles';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Button from '../ui/Button';
 import { useRecoilValue } from 'recoil';
 import { color } from '../../recoil/Color/atom';
 
 export default function Header() {
+  const navigate = useNavigate();
   const { main } = useRecoilValue(color);
 
   return (
@@ -16,7 +17,15 @@ export default function Header() {
       <BtnBox>
         <Button text={'My page'} fontSize={1.4} bgColor={main} width={13} height={4} color={'white'} />
         <Button text={'Guide'} fontSize={1.4} bgColor={main} width={13} height={4} color={'white'} />
-        <Button text={'Sign in'} fontSize={1.4} bgColor={main} width={13} height={4} color={'white'} />
+        <Button
+          onClick={() => navigate('/signup')}
+          text={'Sign in'}
+          fontSize={1.4}
+          bgColor={main}
+          width={13}
+          height={4}
+          color={'white'}
+        />
       </BtnBox>
     </HeaderWrapper>
   );
