e2d2d3583cfcc138715ce1860b55f590cbd7f433
5hyun <qhslsl@gmail.com>
2023-04-07 19:50:01 +0900

    Feat: 기본 상자 컴포넌트

---
+++ src/components/ui/BaseBgBox/styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const BaseGbBoxWrapper = styled.section`
+  width: 70%;
+  margin: 0 auto;
+  margin-top: 4rem;
+
+  border: 1px solid var(--color-light-purple);
+  border-radius: 9rem;
+
+  background-color: rgba(232, 211, 255, 0.2);
+`;
