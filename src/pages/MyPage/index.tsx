bc47f23b1d76952c4487c08766b0390edd7020e9
Chae Young <oco6029@naver.com>
2023-04-08 18:31:35 +0900

    Feat: User 정보페이지

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,3 +1,49 @@
+import { useRecoilValue } from 'recoil';
+import BaseBgBox from '../../components/ui/BaseBgBox';
+import Button from '../../components/ui/Button';
+import { color } from '../../recoil/Color/atom';
+
+import { MyPageWrapper, UserInfo } from './styles';
+import { useEffect, useState } from 'react';
+import { SignUpInfo } from '../../typings/user';
+
 export default function MyPage() {
-  return <>마이페이지</>;
+  //색깔
+  const { subBlue } = useRecoilValue(color);
+
+  // user 개인 정보
+  const [userInfo, setUserInfo] = useState<SignUpInfo>({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  // userInfo 값이 바뀔 때 마다 render
+  useEffect(() => {
+    const temp = { ...userInfo };
+    setUserInfo(temp);
+  }, [userInfo.name, userInfo.email]);
+
+  // 화면에 보여지는 값
+  const { name, email } = userInfo;
+
+  return (
+    <BaseBgBox>
+      <MyPageWrapper>
+        <div>
+          My Page
+          <Button color={'black'} bgColor={subBlue} fontSize={1.3} width={11} height={3}>
+            프로필 수정
+          </Button>
+        </div>
+        <UserInfo>
+          <img src="userProfile.png" alt="userProfile" />
+          <div>
+            <span> 이름 : {name} </span>
+            <span> email : {email} </span>
+          </div>
+        </UserInfo>
+      </MyPageWrapper>
+    </BaseBgBox>
+  );
 }
--- src/pages/MyPage/styles.tsx
+++ src/pages/MyPage/styles.tsx
@@ -1,3 +1,57 @@
 import styled from 'styled-components';
 
-export {};
+export const MyPageWrapper = styled.div`
+  padding: 5rem;
+  display: flex;
+  flex-direction: column;
+
+  & > div:first-of-type {
+    margin-left: 3.5rem;
+    font-size: 3rem;
+    font-weight: 700;
+    display: flex;
+    flex-direction: row;
+
+    & > button {
+      margin-left: 65rem;
+    }
+  }
+`;
+
+export const UserInfo = styled.div`
+  width: 30%;
+  height: 38rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  box-sizing: border-box;
+  padding: 2rem;
+  margin-top: 4.5rem;
+  margin-left: 3.5rem;
+  margin-right: 1rem;
+
+  border-radius: 3rem;
+  background-color: rgba(232, 211, 255, 0.6);
+
+  img {
+    width: 70%;
+    margin-top: 1.8rem;
+  }
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    & > span {
+      margin-bottom: 2rem;
+
+      font-size: 1.8rem;
+      font-weight: 500;
+    }
+  }
+`;
