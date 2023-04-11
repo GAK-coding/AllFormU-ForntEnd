7722930d32d51465933cc149d8a974cacf409340
5hyun <qhslsl@gmail.com>
2023-04-11 21:37:27 +0900

    Feat: ResForm 페이지 초기 설정

---
+++ src/pages/ResForm/index.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import BaseBgBox from '../../components/ui/BaseBgBox';
+import ResFormModal from '../../components/ResFormModal';
+
+export default function ResForm() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  return (
+    <BaseBgBox>
+      <button onClick={showModal}>모달 버튼</button>
+      {isModalOpen && <ResFormModal open={isModalOpen} />}
+    </BaseBgBox>
+  );
+}
