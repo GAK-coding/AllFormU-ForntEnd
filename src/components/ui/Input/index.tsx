2c25e5fdeb98bb11a4cd81b10ba4dc6ee257cdbd
5hyun <qhslsl@gmail.com>
2023-04-14 22:39:17 +0900

    Chore: width, height 옵셔널로 변경

--- src/components/ui/Input/index.tsx
+++ src/components/ui/Input/index.tsx
@@ -4,18 +4,20 @@ import { BaseInput } from './styles';
 export interface Input {
   value: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
   placeholder: string;
-  width: number;
-  height: number;
+  width?: number;
+  height?: number;
   type?: string;
 }
 
-export default function Input({ value, onChange, placeholder, width, height, type }: Input) {
+export default function Input({ value, onChange, onKeyDown, placeholder, width, height, type }: Input) {
   return (
     <BaseInput
       type={type ? type : 'text'}
       value={value}
       onChange={onChange}
+      onKeyDown={onKeyDown}
       placeholder={placeholder}
       width={width}
       height={height}
