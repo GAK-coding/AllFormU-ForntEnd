5c0e57d7ff06238dc5b9e54d0510f348b0e8de5f
5hyun <qhslsl@gmail.com>
2023-04-16 18:29:28 +0900

    Style: 채팅 스타일 줄바꿈 수정

--- src/pages/ResForm/ChatBox/styles.tsx
+++ src/pages/ResForm/ChatBox/styles.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const BallonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
   margin: 1rem 2rem 2rem 1rem;
   padding: 2rem;
   font-weight: 700;
@@ -11,6 +14,8 @@ export const BallonWrapper = styled.div`
   }
 
   & > div {
+    max-width: 50%;
+
     border-radius: 5rem;
     box-shadow: 3px 2px 5px 0px rgba(0, 0, 0, 0.75);
     -webkit-box-shadow: 3px 2px 5px 0px rgba(0, 0, 0, 0.75);
@@ -19,21 +24,16 @@ export const BallonWrapper = styled.div`
     & > span {
       display: flex;
       align-items: center;
-      width: 80%;
       padding: 0.5rem;
     }
   }
 `;
 export const UserBallon = styled.div`
-  display: block;
-
-  float: right;
+  align-self: flex-start;
   background-color: #f7e9a6;
 `;
 
 export const BotBallon = styled.div`
-  display: block;
-
-  float: left;
+  align-self: flex-end;
   background-color: #eeeeee;
 `;
