dabb66df201c76af501bb9380cb8e01fa8161134
5hyun <qhslsl@gmail.com>
2023-04-27 23:38:58 +0900

    Feat: 설문 생성에서 드래그 드랍 원하는 부분에서만 가능하도록 구현(완전 기초만)

--- src/pages/MakeForm/Direct/index.tsx
+++ src/pages/MakeForm/Direct/index.tsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { SortablePane, Pane } from 'react-sortable-pane';
 
 export default function MakeFormDirect() {
-  return <div>직접</div>;
+  const [isClick, setIsClick] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsClick(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsClick(false);
+  };
+
+  console.log(isClick);
+
+  return (
+    <div style={{ fontSize: '4rem' }}>
+      <SortablePane isSortable={isClick} direction="vertical" margin={20}>
+        {[0, 1, 2].map((key) => (
+          <div key={key} style={{ border: '1px solid', width: '100rem' }}>
+            <Pane key={key}>
+              <div
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+                style={{ border: '1px solid', width: '50%' }}
+              >
+                00{key}
+              </div>
+            </Pane>
+          </div>
+        ))}
+      </SortablePane>
+    </div>
+  );
 }
