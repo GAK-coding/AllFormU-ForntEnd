be833195f854b03ccdde6f858efbf65e171c9eb4
5hyun <qhslsl@gmail.com>
2023-04-30 22:44:09 +0900

    Feat: react-beatiful-dnd로 변경해서 전체적인 변경

--- src/pages/MakeForm/Direct/index.tsx
+++ src/pages/MakeForm/Direct/index.tsx
@@ -1,47 +1,49 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { SortablePane, Pane } from 'react-sortable-pane';
+import React, { useCallback, useState } from 'react';
 import { Col, Row } from 'antd';
 import { useRecoilState } from 'recoil';
-import { RxDragHandleHorizontal } from 'react-icons/rx';
-import FormTitle from '../../../components/Questions/FormTitle';
-import ShortQuestion from '../../../components/Questions/ShortQuestion';
-import { AddQuestion, BtnWrapper, DirectWrapper, Drag, QuestionWrapper } from './styles';
+import { AddQuestion, BtnWrapper } from './styles';
 import { questions } from '../../../recoil/MakeForm/atom';
-import { ShortQue } from '../../../typings/makeForm';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import ShortQuestion from '../../../components/Questions/ShortQuestion';
+import FormTitle from '../../../components/Questions/FormTitle';
+import { v4 as uuid } from 'uuid';
 
 export default function MakeFormDirect() {
-  const [isClick, setIsClick] = useState(false);
   const [questionList, setQuestionList] = useRecoilState(questions);
   const [nowIndex, setNowIndex] = useState(0);
-  const [order, setOrder] = useState<number[]>([]);
-
-  const handleMouseOver = () => setIsClick(true);
-  const handleMouseOut = () => setIsClick(false);
 
   const addQuestion = useCallback(() => {
     const temp = [...questionList];
-    temp.splice(nowIndex + 1, 0, { type: 'short', require: false, title: '', answer: '' });
+    temp.splice(nowIndex + 1, 0, { type: 'short', id: uuid(), require: false, title: '', answer: '' });
 
     setQuestionList(temp);
     setNowIndex((prev) => prev + 1);
   }, [nowIndex, questionList]);
 
-  const [isDragging, setIsDragging] = useState(false);
+  const onDragEnd = useCallback(
+    (result: any) => {
+      const temp = [...questionList];
 
-  function sortByOrder(a: number[], b: ShortQue[]) {
-    const orderMap: Record<number, number> = {};
-    for (let i = 0; i < a.length; i++) {
-      orderMap[a[i]] = i;
-    }
+      const start = result.source.index;
+      const end = result.destination.index;
 
-    const sortedB = b.sort((x, y) => orderMap[b.indexOf(x)] - orderMap[b.indexOf(y)]);
-    return sortedB;
-  }
+      const [remove] = temp.splice(start, 1);
+      temp.splice(end, 0, remove);
 
-  useEffect(() => {
-    if (order.length === 0) return;
-    setQuestionList(sortByOrder(order, [...questionList]));
-  }, [order]);
+      setQuestionList(temp);
+      // console.log('이거: ', start, end);
+    },
+    [questionList]
+  );
+
+  const onDelete = useCallback(
+    (index: number) => {
+      const temp = [...questionList];
+      temp.splice(index, 1);
+      setQuestionList(temp);
+    },
+    [questionList]
+  );
 
   console.log(questionList);
 
@@ -49,34 +51,32 @@ export default function MakeFormDirect() {
     <Row>
       <Col span={4} />
       <Col span={16}>
-        <DirectWrapper>
-          <FormTitle />
-          <SortablePane
-            onDragStart={(e, ui) => {
-              setIsDragging(true);
-              console.log('드래그 시작: ', ui);
-            }}
-            onDragStop={(e, ui) => {
-              setIsDragging(false);
-              console.log('드래그 끝: ', ui);
-            }}
-            onOrderChange={(e) => setOrder(e.map((str) => Number(str)))}
-            isSortable={isClick}
-            direction="vertical"
-            margin={20}
-          >
-            {questionList.map((key, index) => (
-              <QuestionWrapper key={index} onClick={() => setNowIndex(index)}>
-                <Pane resizable={{ x: false, y: false, xy: false }} key={key.title}>
-                  <Drag onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-                    <RxDragHandleHorizontal />
-                  </Drag>
-                </Pane>
-                <ShortQuestion data={key} index={index} click={index === nowIndex} />
-              </QuestionWrapper>
-            ))}
-          </SortablePane>
-        </DirectWrapper>
+        <FormTitle />
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="card" type="card" direction="vertical">
+            {(provided) => (
+              <div>
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  {questionList.map((card, idx) => (
+                    <Draggable draggableId={card.id} index={idx} key={card.id}>
+                      {(provided) => (
+                        <div
+                          onClick={() => setNowIndex(idx)}
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <ShortQuestion data={card} index={idx} click={idx === nowIndex} onDelete={onDelete} />
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </div>
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </Col>
       <Col span={4}>
         <BtnWrapper>
