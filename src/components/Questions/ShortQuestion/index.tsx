7493c6bec90965124705d9a68700f3c7021201ac
5hyun <qhslsl@gmail.com>
2023-04-30 21:08:24 +0900

    Feat: 단답형 질문 생성

--- src/components/Questions/ShortQuestion/index.tsx
+++ src/components/Questions/ShortQuestion/index.tsx
@@ -1,16 +1,103 @@
-import React from 'react';
+import React, { ChangeEvent, useCallback } from 'react';
 import { BaseQuestionWrapper } from '../BaseQuestion/styles';
-import { ShortQuestionWrapper } from './styles';
+import {
+  CheckMark,
+  ShortQueBottom,
+  ShortQueBottomLeft,
+  ShortQuestionWrapper,
+  ShortQueTop,
+  ShortQueTopLeft,
+  ShortQueTopRight,
+} from './styles';
+import { ShortQue } from '../../../typings/makeForm';
+import FormInput from '../../ui/FormInput';
+import Button from '../../ui/Button';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { color } from '../../../recoil/Color/atom';
+import { Select } from 'antd';
+import { TbTriangleInverted } from 'react-icons/tb';
+import { GrFormClose } from 'react-icons/gr';
+import { questions } from '../../../recoil/MakeForm/atom';
 
 interface Props {
-  data: any;
+  data: ShortQue;
+  index: number;
+  click: boolean;
 }
 
-export default function ShortQuestion({ data }: Props) {
+type QueType = {
+  value: string;
+  label: string;
+};
+
+const Types: QueType[] = [
+  { value: 'short', label: '단답형' },
+  { value: 'long', label: '장문형' },
+];
+
+export default function ShortQuestion({ data, index, click }: Props) {
+  const { blue } = useRecoilValue(color);
+  const { title, answer } = data;
+  const [questionList, setQuestionList] = useRecoilState(questions);
+
+  const onChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>, name: 'title' | 'answer') => {
+      const temp = JSON.parse(JSON.stringify(questionList));
+      temp[index][name] = e.target.value;
+      console.log(temp);
+      setQuestionList(temp);
+    },
+    [questionList, data]
+  );
+
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+  };
+
   return (
     <BaseQuestionWrapper>
       <ShortQuestionWrapper>
-        <div>{data.title}</div>
+        {click && <CheckMark />}
+        <ShortQueTop>
+          <ShortQueTopLeft>
+            <FormInput
+              value={title}
+              onChange={(e) => onChange(e, 'title')}
+              width={'75%'}
+              fontSize={1.8}
+              placeholder={'질문'}
+            />
+          </ShortQueTopLeft>
+          <ShortQueTopRight>
+            <Select
+              className="custom-select"
+              defaultValue="단답형"
+              style={{ width: 100 }}
+              onChange={handleChange}
+              options={Types}
+              suffixIcon={<TbTriangleInverted />}
+            />
+            <span>
+              <GrFormClose />
+            </span>
+          </ShortQueTopRight>
+        </ShortQueTop>
+        <ShortQueBottom>
+          <ShortQueBottomLeft>
+            <FormInput
+              value={answer}
+              onChange={(e) => onChange(e, 'answer')}
+              width={'75%'}
+              fontSize={1.8}
+              placeholder={'단답형 텍스트'}
+            />
+            <div>
+              <Button color={'black'} bgColor={blue} fontSize={1.4} width={8} height={3.5}>
+                추가
+              </Button>
+            </div>
+          </ShortQueBottomLeft>
+        </ShortQueBottom>
       </ShortQuestionWrapper>
     </BaseQuestionWrapper>
   );
