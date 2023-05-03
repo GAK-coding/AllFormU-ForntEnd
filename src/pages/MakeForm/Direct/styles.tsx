510be04f958946547242c1dcf73ad30f6b72a94f
5hyun <qhslsl@gmail.com>
2023-05-03 10:21:32 +0900

    Feat: 설문 직접 생성 페이지 nowIndex 버그 수정 등 정리

--- src/pages/MakeForm/Direct/styles.tsx
+++ src/pages/MakeForm/Direct/styles.tsx
@@ -1,37 +1,6 @@
 import styled from 'styled-components';
 
-export const DirectWrapper = styled.div`
-  font-size: 4rem;
-`;
-
-export const Drag = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  margin: 0 auto;
-  width: 8rem;
-  height: 3rem;
-
-  font-size: 2.5rem;
-  cursor: grab;
-
-  border: 1px solid black;
-  border-bottom: none;
-  border-radius: 1rem 1rem 0 0;
-
-  background-color: #ececec;
-`;
-
-export const QuestionWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  width: 100%;
-  margin-bottom: 2rem;
-`;
-
-export const BtnWrapper = styled.div``;
+export const DirectForm = styled.form``;
 
 export const AddQuestion = styled.button`
   width: 4rem;
