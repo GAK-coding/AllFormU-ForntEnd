2be25e3328106f315dfe3dbb5f0fa09170340c4f
5hyun <qhslsl@gmail.com>
2023-05-05 01:55:33 +0900

    Chore: dummy 데이터 수정

--- src/recoil/MakeForm/atom.ts
+++ src/recoil/MakeForm/atom.ts
@@ -18,18 +18,6 @@ import {
 } from '../../typings/makeForm';
 import { v4 as uuid } from 'uuid';
 
-// export const DESCRIPTION_SHORT = 'Description_SHORT';
-// export const DESCRIPTION_LONG = 'Description_LONG';
-// export const DESCRIPTION_DATE = 'Description_DATE';
-// export const DESCRIPTION_TIME = 'Description_TIME';
-// export const DESCRIPTION_IMG = 'Description_IMG';
-// export const SELECTION_OPTION = 'Selection_OPTION';
-// export const SELECTION_CHECKBOX = 'Selection_CHECKBOX';
-// export const SELECTION_DROPDOWN = 'Selection_DROPDOWN';
-// export const SELECTION_LINEAR = 'Selection_LINEAR';
-// export const GRID_RADIO = 'Grid_RADIO';
-// export const GRID_CHECKBOX = 'Grid_CHECKBOX';
-
 export const questionTypes = atom({
   key: 'questionTypes',
   default: {
@@ -65,42 +53,6 @@ export const questions = atom<Array<DescriptionQue | SelectionQue | GridQue>[]>(
         title: 'What is your age?',
         options: [{ content: '18' }, { content: '19' }, { content: '20' }],
         sectionNum: 0,
-      },
-    ],
-    [
-      {
-        type: SELECTION_CHECKBOX,
-        id: uuid(),
-        required: false,
-        title: '',
-        options: [{ content: '' }],
-        sectionNum: 1,
-      },
-      {
-        type: SELECTION_CHECKBOX,
-        id: uuid(),
-        required: false,
-        title: '',
-        options: [{ content: '' }],
-        sectionNum: 1,
-      },
-    ],
-    [
-      {
-        type: SELECTION_CHECKBOX,
-        id: uuid(),
-        required: false,
-        title: '123',
-        options: [{ content: '' }],
-        sectionNum: 2,
-      },
-      {
-        type: SELECTION_CHECKBOX,
-        id: uuid(),
-        required: false,
-        title: '',
-        options: [{ content: '' }],
-        sectionNum: 2,
       },
     ],
   ],
