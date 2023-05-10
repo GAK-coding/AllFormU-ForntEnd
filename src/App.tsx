c2a494e12761ea219c317bf1de5381bdee89358f
5hyun <qhslsl@gmail.com>
2023-05-10 14:51:29 +0900

    Feat: reform url 생성

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
-import ResForm from './pages/ResForm';
 import MakeForm from './pages/MakeForm/Main';
 import MakeFormSelect from './pages/MakeForm/Select';
 import MakeFormDirect from './pages/MakeForm/Direct';
@@ -17,6 +16,8 @@ import Edit from './pages/MyPage/Edit';
 import FindPassword from './pages/FindPassword';
 import ResFormList from './pages/DetailForm/ResFomList';
 import FixedButton from './components/FixedButton';
+import ChatbotResForm from './pages/ResForm/ChatbotResForm';
+import DirectResForm from './pages/ResForm/DirectResForm';
 
 const queryClient = new QueryClient();
 
@@ -36,7 +37,8 @@ function App() {
             <Route path={'/mypage/resform'} element={<ResFormList />} />
             <Route path={'/signin'} element={<SignIn />} />
             <Route path={'/signin/findpassword'} element={<FindPassword />} />
-            <Route path={'/resform'} element={<ResForm />} />
+            <Route path={'/resform/chatbot'} element={<ChatbotResForm />} />
+            <Route path={'/resform/direct/:id'} element={<DirectResForm />} />
             <Route path={'/makeform'} element={<MakeForm />} />
             <Route path={'/makeform/select'} element={<MakeFormSelect />} />
             <Route path={'/makeform/direct'} element={<MakeFormDirect />} />
