d2e40ed7a02aaa38935359737282c8283ace598d
5hyun <qhslsl@gmail.com>
2023-05-13 03:13:58 +0900

    Feat: 폼 제목, 설명 수정을 위한 기능 추가

--- src/components/Questions/FormTitle/index.tsx
+++ src/components/Questions/FormTitle/index.tsx
@@ -1,12 +1,23 @@
-import React, { ChangeEvent, useCallback } from 'react';
+import React, { ChangeEvent, useCallback, useRef } from 'react';
 import { useRecoilState } from 'recoil';
 import { formInfo } from '../../../recoil/MakeForm/atom';
 import { FormTitleWrapper, TitleInput } from './styles';
 import TextArea from 'antd/es/input/TextArea';
 import { QueWrapper } from '../MakeQueBase/styles';
+import { useMutation, useQuery } from 'react-query';
+import { GetFormInfo } from '../../../typings/getForm';
+import { getFormInfo } from '../../../api/getFormInfo';
+import { editFormInfo } from '../../../api/editForm';
+import { debounce } from 'lodash';
 
-export default function FormTitle() {
+interface Props {
+  isEdit?: boolean;
+  formId?: string;
+}
+
+export default function FormTitle({ isEdit, formId }: Props) {
   const [info, setInfo] = useRecoilState(formInfo);
+  const { mutate, isLoading, error, isError } = useMutation(() => editFormInfo(1, +formId!, info.title, info.content));
 
   const onChangeTitle = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
@@ -22,11 +33,27 @@ export default function FormTitle() {
     [info]
   );
 
+  const onBlur = useCallback(
+    debounce(() => {
+      if (isEdit) {
+        mutate();
+      }
+    }, 3000),
+    [isEdit, info]
+  );
+
   return (
     <QueWrapper style={{ backgroundColor: '#F5F5F5' }}>
       <FormTitleWrapper>
-        <TitleInput value={info.title} onChange={onChangeTitle} placeholder={'설문 제목 입력'} required />
+        <TitleInput
+          onBlur={onBlur}
+          value={info.title}
+          onChange={onChangeTitle}
+          placeholder={'설문 제목 입력'}
+          required
+        />
         <TextArea
+          onBlur={onBlur}
           showCount
           maxLength={300}
           style={{ height: 80, resize: 'none' }}
