b3f52ead40c45a4b3f8ab8c4caf760aa6675538a
5hyun <qhslsl@gmail.com>
2023-05-13 03:13:29 +0900

    Chore: 백엔드에서 받는 값 수정중

--- src/typings/getForm.ts
+++ src/typings/getForm.ts
@@ -1,3 +1,5 @@
+import { DescriptionQue, FormInfo, SelectionQue } from './makeForm';
+
 interface Question {
   createdDate: string;
   modifiedDate: string;
@@ -34,4 +36,30 @@ export interface GetForm {
   content: string | null;
   required: boolean;
   fix: boolean;
+}
+
+// getFormInfo
+export interface GetFormInfo extends FormInfo {
+  id: number;
+  questions: (GetDescription | GetSelection)[];
+  fix: boolean;
+}
+
+interface GetDescription extends Omit<DescriptionQue, 'id' | 'descriptions'> {
+  id: number;
+  descriptions: {
+    id: number;
+    content: string;
+    answer: null | string;
+    quiz: boolean;
+  };
+}
+
+interface GetSelection extends Omit<SelectionQue, 'id' | 'options'> {
+  id: number;
+  options: Array<{
+    id: number;
+    content: string;
+    answer: boolean;
+  }>;
 }
