a01a7dc7fec6b2b2981c9705ef6d5a015b541268
5hyun <qhslsl@gmail.com>
2023-05-14 15:25:45 +0900

    Chore: 처음 폼 만들면 데이터 초기화

--- src/pages/MyPage/EditForm/index.tsx
+++ src/pages/MyPage/EditForm/index.tsx
@@ -1,12 +1,28 @@
-import React, { useCallback, useRef } from 'react';
+import React, { ChangeEvent, FormEvent, useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { getFormInfo } from '../../../api/getFormInfo';
 import { GetFormInfo } from '../../../typings/getForm';
 import MakeFormDirect from '../../MakeForm/Direct';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { formInfo, questions } from '../../../recoil/MakeForm/atom';
-import { DescriptionQue, GridQue, SelectionQue } from '../../../typings/makeForm';
+import {
+  formInfo,
+  nowFocusIndex,
+  nowQuestion,
+  queSectionNum,
+  questions,
+  questionTypes,
+  sectionLens,
+  sectionNames,
+} from '../../../recoil/MakeForm/atom';
+import {
+  Description,
+  DESCRIPTION_SHORT,
+  DescriptionQue,
+  GridQue,
+  Option,
+  SelectionQue,
+} from '../../../typings/makeForm';
 import { Col, Row } from 'antd';
 import { AddQuestion, AddSection, DirectForm } from '../../MakeForm/Direct/styles';
 import FormTitle from '../../../components/Questions/FormTitle';
@@ -17,6 +33,8 @@ import MakeFromModal from '../../../components/MakeForm/MakeFromModal';
 import { useMessage } from '../../../hooks/useMessage';
 import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd';
 import { color } from '../../../recoil/Color/atom';
+import { v4 as uuid } from 'uuid';
+
 // import { DescriptionKinds, GridKinds, SelectionKinds } from '../../../typings/makeForm';
 
 // interface GetFormInfo {
@@ -46,12 +64,205 @@ export default function EditForm() {
   const { id } = useParams();
   const [info, setInfo] = useRecoilState(formInfo);
   const [questionList, setQuestionList] = useRecoilState(questions);
+  const queTypes = useRecoilValue(questionTypes);
 
   const { data, isLoading, error, isError } = useQuery<GetFormInfo>('getFormInfo', () => getFormInfo(1, +id!), {
     onSuccess: (data) => {
       setInfo(data);
     },
+    // staleTime: 600000, // 10분
+    // cacheTime: 900000, // 15분
+    // refetchOnMount: false, // 마운트(리렌더링)될 때 데이터를 다시 가져오지 않음
+    // refetchOnWindowFocus: false, // 브라우저를 포커싱했을때 데이터를 가져오지 않음
+    // refetchOnReconnect: false, // 네트워크가 다시 연결되었을때 다시 가져오지 않음
   });
+
+  const customData = (data: GetFormInfo) => {
+    const tempQues: (DescriptionQue | SelectionQue | GridQue)[][] = [];
+
+    data?.questions.map((que) => {
+      const { sectionNum } = que;
+
+      if (tempQues[sectionNum] === undefined) tempQues[sectionNum] = [];
+      tempQues[sectionNum].push({ ...que, tempId: uuid() });
+    });
+
+    return tempQues;
+  };
+
+  useEffect(() => {
+    setQuestionList(customData(data!));
+  }, []);
+
+  // console.log(JSON.stringify(customData(data!).flat()));
+
+  // 각 섹션이 몇 번 인덱스까지 사용하는지
+  const [accrueQue, setAccrueQue] = useRecoilState(sectionLens);
+  const [nowIndex, setNowIndex] = useRecoilState(nowFocusIndex);
+  const [nowQueInfo, setNowQueInfo] = useRecoilState(nowQuestion);
+  const [sectionList, setSectionList] = useRecoilState(sectionNames);
+  const [queSecNum, setQueSecNum] = useRecoilState(queSectionNum);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isCreate, setIsCreate] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  const { showMessage, contextHolder } = useMessage();
+  const { blue } = useRecoilValue(color);
+
+  const showModal = useCallback((e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsModalOpen(true);
+  }, []);
+
+  const handleCancel = useCallback(() => {
+    setIsModalOpen(false);
+  }, []);
+
+  const addQuestion = useCallback(() => {
+    const temp: (DescriptionQue | SelectionQue | GridQue)[][] = JSON.parse(JSON.stringify(questionList));
+    const { row, col } = nowQueInfo;
+
+    let queSum = 0;
+    questionList.map((que) => (queSum += que.length));
+    if (queSum >= 100) {
+      showMessage('warning', '질문은 100개까지만 등록 가능합니다.');
+      return;
+    }
+
+    temp[row].splice(col + 1, 0, {
+      type: DESCRIPTION_SHORT,
+      tempId: uuid(),
+      required: false,
+      title: '',
+      sectionNum: row,
+      descriptions: [{ content: '' }],
+    });
+
+    setQuestionList(temp);
+    setNowIndex((prev) => prev + 1);
+  }, [questionList, nowQueInfo, nowIndex]);
+
+  const addSection = useCallback(() => {
+    const temp: (DescriptionQue | SelectionQue | GridQue)[][] = JSON.parse(JSON.stringify(questionList));
+    const tempSectionList: string[] = JSON.parse(JSON.stringify(sectionList));
+
+    let queSum = 0;
+    questionList.map((que) => (queSum += que.length));
+    if (queSum >= 100) {
+      showMessage('warning', '질문은 100개까지만 등록 가능합니다.');
+      return;
+    }
+
+    if (questionList.length >= 5) {
+      showMessage('warning', '섹션은 5개까지만 등록 가능합니다.');
+      return;
+    }
+
+    temp.push([
+      {
+        type: DESCRIPTION_SHORT,
+        tempId: uuid(),
+        required: true,
+        title: '',
+        sectionNum: temp.length,
+        descriptions: [{ content: '' }],
+      },
+    ]);
+
+    setQuestionList(temp);
+    setQueSecNum((prev) => [...prev, { value: queSecNum.length.toString(), label: (queSecNum.length + 1).toString() }]);
+    setSectionList([...tempSectionList, '']);
+    setNowQueInfo({ row: temp.length - 1, col: 0 });
+    setNowIndex(accrueQue[accrueQue.length - 1] + 1);
+  }, [questionList, nowIndex, accrueQue, nowQueInfo, sectionList, queSecNum]);
+
+  const onChangeTitle = useCallback(
+    (e: ChangeEvent<HTMLInputElement>, name: 'title', row: number, col: number) => {
+      const temp: (DescriptionQue | SelectionQue | GridQue)[][] = JSON.parse(JSON.stringify(questionList));
+      temp[row][col][name] = e.target.value;
+      setQuestionList(temp);
+    },
+    [questionList]
+  );
+
+  const onDragEnd = useCallback(
+    (result: DropResult, row: number) => {
+      const temp: (DescriptionQue | SelectionQue | GridQue)[][] = JSON.parse(JSON.stringify(questionList));
+
+      const start = result.source.index;
+      const end = result?.destination?.index;
+
+      const [remove] = temp[row].splice(start, 1);
+      temp[row].splice(end!, 0, remove);
+
+      setQuestionList(temp);
+      setNowIndex(row === 0 ? end! : accrueQue[row - 1] + 1 + end!);
+      setNowQueInfo({ row: row, col: end! });
+    },
+    [questionList, nowIndex, accrueQue, nowQueInfo]
+  );
+
+  const onDelete = useCallback(
+    (row: number, col: number) => {
+      if (row === 0 && questionList[row].length === 1) {
+        showMessage('warning', '질문은 최소 1개 이상이어야 합니다.');
+        return;
+      }
+
+      const temp: (DescriptionQue | SelectionQue | GridQue)[][] = JSON.parse(JSON.stringify(questionList));
+      const sectionName: string[] = JSON.parse(JSON.stringify(sectionList));
+      const delIdx = row === 0 ? col : accrueQue[row - 1] + col + 1;
+
+      if (row !== 0 && questionList[row].length === 1) {
+        temp.splice(row, 1);
+        sectionName.splice(row, 1);
+        setQuestionList(temp);
+        setSectionList(sectionName);
+        setQueSecNum((prev) => {
+          const temp = [...prev];
+          temp.pop();
+
+          return temp;
+        });
+
+        if (delIdx === nowIndex) {
+          setNowIndex((prev) => prev - 1);
+        }
+        return;
+      }
+
+      temp[row].splice(col, 1);
+      setQuestionList(temp);
+      if (delIdx < nowIndex || (col !== 0 && delIdx === nowIndex)) {
+        setNowIndex((prev) => prev - 1);
+      }
+    },
+    [questionList, nowIndex, accrueQue, sectionList, queSecNum]
+  );
+
+  const onClickQue = useCallback(
+    (row: number, col: number) => {
+      const index = row === 0 ? col : accrueQue[row - 1] + col + 1;
+
+      setNowIndex(index);
+      setNowQueInfo({ row, col });
+    },
+    [nowQueInfo, nowIndex, accrueQue]
+  );
+
+  useLayoutEffect(() => {
+    const temp: number[] = [];
+
+    for (let i = 0; i < questionList.length; i++) {
+      i === 0 ? temp.push(questionList[i].length - 1) : temp.push(temp[i - 1] + questionList[i].length);
+    }
+
+    setAccrueQue(temp);
+  }, [addQuestion]);
+
+  useLayoutEffect(() => {
+    ref.current?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  }, [questionList]);
 
   if (isLoading) {
     return <div>로딩중...</div>;
@@ -61,20 +272,95 @@ export default function EditForm() {
     <Row>
       <Col span={4} />
       <Col span={16}>
-        <DirectForm onSubmit={() => true}>
-          <FormTitle isEdit={true} formId={id} />
+        {contextHolder}
+        <DirectForm onSubmit={showModal}>
+          <FormTitle isEdit={true} formId={id} />{' '}
+          {customData(data!)?.map((section, row) => (
+            <DragDropContext key={`section-${row}`} onDragEnd={(result) => onDragEnd(result, row)}>
+              <SectionBox index={row}>
+                <Droppable droppableId="card" type="card" direction="vertical">
+                  {(provided) => (
+                    <div>
+                      <div {...provided.droppableProps} ref={provided.innerRef}>
+                        {section?.map((que, col) => {
+                          const focus = row === 0 ? col === nowIndex : accrueQue[row - 1] + col + 1 === nowIndex;
+
+                          if (focus) {
+                            return (
+                              <div ref={ref} key={que.tempId}>
+                                <QueDraggable
+                                  draggableId={que.tempId}
+                                  data={que}
+                                  row={row}
+                                  col={col}
+                                  isClick={focus}
+                                  onChangeTitle={onChangeTitle}
+                                  onClickQue={onClickQue}
+                                  onDelete={onDelete}
+                                />
+                              </div>
+                            );
+                          }
+
+                          return (
+                            <div key={que.tempId}>
+                              <QueDraggable
+                                draggableId={que.tempId}
+                                data={que}
+                                row={row}
+                                col={col}
+                                isClick={focus}
+                                onChangeTitle={onChangeTitle}
+                                onClickQue={onClickQue}
+                                onDelete={onDelete}
+                              />
+                            </div>
+                          );
+                        })}
+                      </div>
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              </SectionBox>
+            </DragDropContext>
+          ))}
+          <Button type={'submit'} color={'black'} bgColor={blue} fontSize={1.6} width={14} height={4.5}>
+            폼 생성하기
+          </Button>
         </DirectForm>
 
-        <MakeFromModal isCreate={false} setIsCreate={() => true} open={false} onCancel={() => true} />
+        <MakeFromModal isCreate={isCreate} setIsCreate={setIsCreate} open={isModalOpen} onCancel={handleCancel} />
       </Col>
       <Col span={4}>
-        <AddQuestion onClick={() => true}>
+        <AddQuestion onClick={addQuestion}>
           <span>질문 추가</span>
         </AddQuestion>
-        <AddSection onClick={() => true}>
+        <AddSection onClick={addSection}>
           <span>섹션 추가</span>
         </AddSection>
       </Col>
     </Row>
   );
+  // return <MakeFormDirect />;
+  // return (
+  //   <Row>
+  //     <Col span={4} />
+  //     <Col span={16}>
+  //       <DirectForm onSubmit={() => true}>
+  //         <FormTitle isEdit={true} formId={id} />
+  //       </DirectForm>
+  //
+  //       <MakeFromModal isCreate={false} setIsCreate={() => true} open={false} onCancel={() => true} />
+  //     </Col>
+  //     <Col span={4}>
+  //       <AddQuestion onClick={() => true}>
+  //         <span>질문 추가</span>
+  //       </AddQuestion>
+  //       <AddSection onClick={() => true}>
+  //         <span>섹션 추가</span>
+  //       </AddSection>
+  //     </Col>
+  //   </Row>
+  // );
 }
