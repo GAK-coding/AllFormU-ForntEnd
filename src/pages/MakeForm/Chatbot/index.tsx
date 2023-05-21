bd50a3b2e47caf9cc5174ccf4d87f412aaaf3e87
CHCHAENG <oco6029@naver.com>
2023-05-21 23:56:11 +0900

    Style : 말풍선 스타일 적용

--- src/pages/MakeForm/Chatbot/index.tsx
+++ src/pages/MakeForm/Chatbot/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import BaseBgBox from '../../../components/ui/BaseBgBox';
-import { ChatbotWrapper, InPutWrapper, ViewWrapper, Wrapper } from './styles';
+import {
+  BallonWrapper,
+  ChatBallon,
+  ChatbotWrapper,
+  GAK,
+  InPutWrapper,
+  UserBallon,
+  UserWrapper,
+  ViewWrapper,
+  Wrapper,
+} from './styles';
 
 export default function MakeFormChatbot() {
   return (
@@ -8,13 +18,40 @@ export default function MakeFormChatbot() {
       <Wrapper>
         <ViewWrapper>
           <ChatbotWrapper>
-            <div>
-              챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선
-              1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선
-              1챗봇 말풍선 1
-            </div>
-            <div>챗봇 말풍선 2</div>
+            <BallonWrapper>
+              <GAK>
+                <img src="/images/gak_chatbot.png" alt="gak" />
+                <span>GAK</span>
+              </GAK>
+              <ChatBallon>
+                챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선
+                1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선
+                1챗봇 말풍선 1
+              </ChatBallon>
+            </BallonWrapper>
+            <BallonWrapper>
+              <GAK>
+                <img src="/images/gak_chatbot.png" alt="gak" />
+                <span>GAK</span>
+              </GAK>
+              <ChatBallon>말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1챗봇 말풍선 1</ChatBallon>
+            </BallonWrapper>
+
+            <BallonWrapper>
+              <GAK>
+                <img src="/images/gak_chatbot.png" alt="gak" />
+                <span>GAK</span>
+              </GAK>
+              <ChatBallon>말풍선</ChatBallon>
+            </BallonWrapper>
           </ChatbotWrapper>
+          <UserWrapper>
+            <UserBallon>
+              유저 말풍선 1유저 말풍선 1유저 말풍선 1유저 말풍선 1유저 말풍선 1유저 말풍선 1유저 말풍선 1유저 말풍선
+              1유저 말풍선 1유저 말풍선 1유저 말풍선 1유저 말풍선 1유저 말풍선 1유저 말풍선 1유저 말풍선 1유저 말풍선
+              1유저 말풍선 1유저 말풍선 1유저 말풍선 1유저 말풍선 1유저 말풍선 1유저 말풍선 1
+            </UserBallon>
+          </UserWrapper>
         </ViewWrapper>
         <InPutWrapper>입력란</InPutWrapper>
       </Wrapper>
--- src/pages/MakeForm/Chatbot/styles.tsx
+++ src/pages/MakeForm/Chatbot/styles.tsx
@@ -4,18 +4,17 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
+  padding: 4rem;
 `;
 
 export const ViewWrapper = styled.div`
   width: 100%;
-  height: 70%;
+  height: 80%;
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
-  align-items: self-start;
   margin: auto;
 
-  padding: 4rem;
+  padding-top: 2rem;
   overflow-y: scroll;
 
   &::-webkit-scrollbar {
@@ -25,19 +24,128 @@ export const ViewWrapper = styled.div`
 
 export const ChatbotWrapper = styled.div`
   width: 50%;
-  display: grid;
-  grid-template-columns: 50% 50%;
-
-  grid-template-rows: 50% 50%;
+  display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: self-start;
-  margin: auto;
+  margin-right: auto;
+`;
+
+export const BallonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 2rem;
+  width: 100%;
+`;
+
+export const GAK = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 10%;
+  margin-top: auto;
+  margin-bottom: 2.5rem;
+  margin-right: 2rem;
+
+  & > img {
+    width: 100%;
+    margin-bottom: 1rem;
+  }
+
+  & > span:first-of-type {
+    font-size: 1.5rem;
+    font-weight: 600;
+    align-items: center;
+    margin: auto;
+  }
+`;
+
+export const ChatBallon = styled.div`
+  position: relative;
+  font-size: 16px;
+  font-weight: 600;
+  /* line-height: 2.4rem; */
+  max-width: 75%;
+  border-radius: 4rem;
+  padding: 3rem;
+  text-align: left;
+  color: #2d2d2d;
+  background: #eeeeee;
+  margin-bottom: 1.5rem;
+  word-wrap: break-word;
+
+  margin-left: 1rem;
+
+  & > span {
+    border: 1px solid;
+  }
+
+  &:before {
+    content: '';
+    /* width: 0px;
+    height: 0px; */
+    position: absolute;
+    border-left: 24px solid #eeeeee;
+    border-right: 12px solid transparent;
+    border-top: 12px solid #eeeeee;
+    border-bottom: 20px solid transparent;
+    left: -6px;
+    bottom: -8px;
+    transform: rotate(30deg);
+  }
+`;
+
+export const UserWrapper = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: self-end;
+  margin-left: auto;
+
+  & > div {
+    display: flex;
+    flex-direction: row;
+  }
+`;
+
+export const UserBallon = styled.div`
+  position: relative;
+  font-size: 16px;
+  font-weight: 600;
+  /* line-height: 2.4rem; */
+  width: auto;
+  background: #f7e9a6;
+  border-radius: 4rem;
+  padding: 2rem;
+  text-align: center;
+  color: #2d2d2d;
+  background-color: #f7e9a6;
+  margin-bottom: 3rem;
+  word-wrap: break-word;
+  margin-right: 1rem;
+
+  & > span {
+    border: 1px solid;
+  }
+
+  &:before {
+    content: '';
+    width: 0px;
+    height: 0px;
+    position: absolute;
+    border-right: 24px solid #f7e9a6;
+    border-left: 12px solid transparent;
+    border-top: 12px solid #f7e9a6;
+    border-bottom: 20px solid transparent;
+    right: -5px;
+    bottom: -20px;
+    transform: rotate(-30deg);
+  }
 `;
 
 export const InPutWrapper = styled.div`
   width: 100%;
-  height: 30%;
+  height: 20%;
   display: flex;
   flex-direction: column;
   justify-content: center;
