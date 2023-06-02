b6ca27b12ece04a6cf22f7770c0aaaa36f282673
5hyun <qhslsl@gmail.com>
2023-06-02 12:29:22 +0900

    Feat: Description 응답 저장 되게 함

--- src/components/Form/DirectResForm/DescriptionTypes/Long/index.tsx
+++ src/components/Form/DirectResForm/DescriptionTypes/Long/index.tsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { ChangeEvent, useCallback, useEffect, useState } from 'react';
 import TextArea from 'antd/es/input/TextArea';
 import { LongWrapper } from './styles';
+import { useRecoilState } from 'recoil';
+import { resDescriptionSets } from '../../../../../recoil/Resform/atom';
+import { ResDescription } from '../../../../../typings/resForm';
 
 interface Props {
   id: number;
 }
 
 export default function Long({ id }: Props) {
+  const [resData, setResData] = useRecoilState(resDescriptionSets);
+  const [idx, setIdx] = useState<number>(-1);
+
+  const onChange = useCallback(
+    (e: ChangeEvent<HTMLTextAreaElement>) => {
+      const temp = JSON.parse(JSON.stringify(resData));
+      (temp[idx] as ResDescription).content = e.target.value;
+      setResData(temp);
+    },
+    [idx, resData]
+  );
+
+  useEffect(() => {
+    resData.find((que, index) => {
+      if ('question_id' in que && que.question_id === id) setIdx(index);
+    });
+  }, [resData, idx]);
+
   return (
     <LongWrapper>
       <TextArea
         showCount
         maxLength={100}
         style={{ height: 80, resize: 'none' }}
-        value={''}
-        onChange={() => true}
+        value={idx !== -1 ? (resData[idx] as ResDescription)['content'] : ''}
+        onChange={onChange}
         placeholder="내 답변"
         required
       />
