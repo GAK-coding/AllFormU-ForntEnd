0fc8f64697dc06b3e5121585200d499efdac92bd
5hyun <qhslsl@gmail.com>
2023-06-04 17:51:39 +0900

    Style: 응답 ui 수정

---
+++ src/components/Form/DirectResForm/ResFormInfo/styles.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const ResFormInfoWrapper = styled.div`
+  padding: 2rem;
+  padding-top: 0;
+  color: #696969;
+
+  & > h1 {
+    font-size: 2.6rem;
+
+    padding-bottom: 1rem;
+    border-bottom: 1px solid #696969;
+  }
+
+  & > img {
+    width: 80%;
+  }
+
+  & > div {
+    margin-top: 1rem;
+    font-size: 1.6rem;
+    font-weight: 700;
+  }
+`;
